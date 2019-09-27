488deaa81b3ee0424ce9bc3df7e046363ac871eb
hyp <igor.holanda.main@gmail.com>
2019-09-27 20:38:31 -0300

    upload do pdf

--- calculadora_recursiva.py
+++ calculadora_recursiva.py
@@ -4,9 +4,11 @@
 #Descricao  Calculadora recursiva
 
 import funcoes_matematicas as mat
+#import do modulo criado para lidar com as operacoes
+
 
 def debug():
-    mult = mat.multiplicacao(-3,3)
+    mult = mat.multiplicacao(3,3)
     print(mult)
 
 debug()
