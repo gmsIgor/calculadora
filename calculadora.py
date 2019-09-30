f5e03901ec981d04a1b31128c8cd111b7b5a58ef
hyp <igor.holanda.main@gmail.com>
2019-09-30 02:06:14 -0300

    final commit

--- calculadora.py
+++ calculadora.py
@@ -1,34 +1,81 @@
+#---------------------------------------------------------------------------------------------------
 #Autor Igor Gomes Pereira
 #Data 01.10.19
 #
 #Descricao  Calculadora recursiva
+#---------------------------------------------------------------------------------------------------
+
 
 import funcoes_matematicas as mat
 #import do modulo criado para lidar com as operacoes
+
+import os
+
+
 
 def menu():
     print('--------------------------------------------------------')
     print('que operacao deseja fazer?')
     print('     (1-   |multiplicacao')
     print('     (2-   |divisao')
-    print('     (3-   |achar o n primeiros termos da sequencia de fibonacci')
-    print('     (4-   |MDC entre 2 numeros inteiros)')
-    print('     (5-   |achar os primeiros n numeros primos')
+    print('     (3-   |achar o \'n\' primeiros termos da sequencia de fibonacci')
+    print('     (4-   |MDC entre dois numeros inteiros)')
+    print('     (5-   |achar os primeiros \'n\' numeros primos')
     print('--------------------------------------------------------')
     opcao = int(input('[digite -1 para sair]-(: '))
 
-def main():
-    return 
-    #menu()
+    if opcao == -1:
+        return 'sair'
 
+    elif opcao == 1:
+        print('-------[voce escolheu a multiplicacao]-------')
+        n1 = int(input('[entre com o primeiro valor]-(: '))
+        n2 = int(input('[entre com o segundo valor]-(: '))
+        return mat.multiplicacao(n1,n2)
+
+    elif opcao == 2:
+        print('-------[voce escolheu a divisao]-------')
+        n1 = int(input('[entre com o dividendo]-(: '))
+        n2 = int(input('[entre com o divisor]-(: '))
+        print('\n\n\n\n-------[resultado em forma de: \'<quociente>, <resto>\']-------')
+        return mat.divivisao(n1,n2)
+    
+    elif opcao == 3:
+        print('-------[voce escolheu os \'n\' termos da sequencia de fibonacci]-------')
+        n = int(input('[entre com o \'n\']-(: '))
+        return mat.fibo_seq(n)
+    elif opcao == 4:
+        print('-------[voce escolheu o MDC entre dois numeros inteiros]-------')
+        n1 = int(input('[entre com o primeiro valor]-(: '))
+        n2 = int(input('[entre com o segundo valor]-(: '))
+        return mat.mdc(n1,n2)
+    elif opcao == 5:
+        print('-------[voce escolheu os \'n\' primeiros numeros primos]-------')
+        n = int(input('[entre com o \'n\']-(: '))
+        return mat.primos(n)
+
+def main():
+
+    while True:
+        resultado = menu()
+        if resultado == 'sair':
+            print('-------[voce escolheu sair do programa]-------')
+            return 0
+
+        resultado_str = str(resultado).replace('[','',1)
+        resultado_str = resultado_str.replace(']','.',1)
+        print('\n\n-------[RESULTADO]-------')
+        print(resultado_str)
+        print('-------[RESULTADO]-------\n\n\n\n\n')
+        
 main()
 
+#demonstracao do poder da otimizacao
+def demonstrativo():
 
-def debug():
-    mult = mat.multiplicacao(3,3)
-    print(mult, 'mult')
+    naci = mat.fibo_seq(3000)
 
-    div = mat.divivisao(40,2)
-    print(div, 'div')
+    print(len(naci))
+    print('terminou')
 
-debug()
+#demonstrativo()
--- funcoes_matematicas.py
+++ funcoes_matematicas.py
@@ -7,6 +7,8 @@
 #nenhuma funcao considera valores menores que 0
 #---------------------------------------------------------------------------------------------------
 
+
+
 #
 def multiplicacao(n1,n2):
         n2 = -abs(n2) #garante que nao seja usada subtracao na funcao
@@ -23,18 +25,93 @@ def multiplicacao(n1,n2):
 #
 def divivisao(dividendo,divisor):
     quociente = 0
-    resto= 0
+    resto = 0
+    
+    if divisor == 0:
+        return 'ERRO: divisao por 0'
 
     while True:
         dividendo -= divisor
         
-        if dividendo < 0:
+        if dividendo < 0:             
             resto = dividendo + divisor      #ao chegar a 0, a sequencia de subtracoes se
             return [quociente, resto]        #encerra retornando o quociente e o resto
             
         quociente += 1
 
 #retorna o n-esimo termo da sequencia de fibonacci
+#   versao que exige muito do poder de processamento
 def fibonacci(n):
+    if n == 2:
+        return 1
+    if n == 1:
+        return 0
+    return fibonacci(n-1) + fibonacci(n-2)
 
-    return
+#baseado no algoritmo de euclides
+def mdc(n1,n2):
+    if (n1 == 0) or (n2 == 0):
+        return 'ERRO: divisao por 0'
+
+    dividendo = max(n1,n2)
+    divisor = min(n1,n2)
+    resto = 0
+    while True:
+        resto = dividendo % divisor
+        dividendo = divisor
+        if resto != 0:
+            divisor = resto
+        else:
+            return divisor
+
+
+#
+def primos(n):
+    primos = []
+    i = 2
+    while i <= n + 1:
+        primo = True
+        for j in range(2,i):
+            if (i%j == 0) and (i != j):     #checa se 'i' e divisivel pelos numeros do conjunto [2,i-1]
+                primo = False
+        
+        if primo:
+            primos += [i]                   #se nao for divisivel pelo conjunto, e um numero primo
+        else:
+            n += 1                          #se for divisivel entao precisamos buscar mais valores, somando 1 ao n
+
+        i+=1                                #muda-se o 'i' a cada nova chamada do loop ate conseguir os 'n' numeros primos
+    return primos
+    
+            
+
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-[extra mile]-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+
+#uma versao otimizada da repeticao do fibonacci (mesmo com recursao).
+#       evita passar chamadas de funcao desnecessarias, pode meter uma sequencia de 1000 
+#       que nao vai demorar nada pra criar a lista (talvez demore um pouco pra printar apenas)
+
+#funcao recursiva
+def fibo_ja_sei(n, ja_sei):
+    if n == ja_sei[0]:
+        return ja_sei[1]
+    if n == ja_sei[0] - 1:
+        return ja_sei[2]
+    
+    return fibo_ja_sei(n-1, ja_sei) + fibo_ja_sei(n-2, ja_sei)
+
+#funcao nao recursiva
+def fibo_seq(fim):
+    fibo_seq = [0,1]
+    ja_sei = [2,1,0]
+    
+    if fim <3:
+        return fibo_seq[:fim]
+
+    for i in range(3,fim + 1):
+        fibo_seq += [fibo_ja_sei(i-1,ja_sei)]
+        ja_sei = [i-1,fibo_seq[i-1],fibo_seq[i-2]]
+    
+    return fibo_seq
+
+
